2ecff6dfc27da3c7a92545e608e14ffa078cbdf3
Chris Zuniga <zunigachristian81@gmail.com>
2021-10-27 14:57:34 -0500

    front-end done

--- server.js
+++ server.js
@@ -4,6 +4,8 @@ const path = require('path');
 const express = require('express');
 const PORT = process.env.PORT || 3001;
 const app = express();
+app.use(express.static('public'));
+
 app.use(express.urlencoded({ extended: true}));
 
 app.use(express.json());
@@ -99,6 +101,22 @@ app.get('/api/animals/:id', (req, res) => {
     } else {
         res.send(404);
     }
+  });
+
+  app.get('/', (req, res) => {
+    res.sendFile(path.join(__dirname, './public/index.html'));
+  });
+
+  app.get('/animals', (req, res) => {
+    res.sendFile(path.join(__dirname, './public/animals.html'));
+  });
+
+  app.get('/zookeepers', (req, res) => {
+    res.sendFile(path.join(__dirname, './public/zookeepers.html'));
+  });
+
+  app.get('*', (req, res) => {
+    res.sendFile(path.join(__dirname, './public/index.html'));
   });
 
   app.post('/api/animals', (req, res) => {
